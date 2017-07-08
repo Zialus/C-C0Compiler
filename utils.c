14ef838111c788c5f7682f2ce34a38e37ff40edc
Raul Ferreira <raulmendesferreira@gmail.com>
2017-07-08 03:49:17 +0100

    STDERR for this makes a lot more sense.

--- utils.c
+++ utils.c
@@ -2,6 +2,6 @@
 
 void check_if_buffer_was_big_enough(int n, int buffer_size){
     if (n >= buffer_size ) {
-        printf("It wasn't big enough!! Buffer had size %d, but you wrote %d chars to it + \'\\0\' termination symbol \n",buffer_size,n);
+        fprintf(stderr,"It wasn't big enough!! Buffer had size %d, but you wrote %d chars to it + \'\\0\' termination symbol \n",buffer_size,n);
     }
 }
