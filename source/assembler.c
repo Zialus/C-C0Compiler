4fa5e2346e4bbe8b2d2b34c95fc1d707702b6bca
Raul Ferreira <raulmendesferreira@gmail.com>
2017-11-25 15:58:12 +0000

    Trying to make this windows compatible

--- source/assembler.c
+++ source/assembler.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <fcntl.h>
 #include <search.h>
+
+#ifdef _WIN32
+#include <io.h>
+#else
+#include <unistd.h>
+#endif _WIN32
 
 #include "print_mips.h"
 #include "tree.h"
